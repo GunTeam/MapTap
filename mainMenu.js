5a1a06dcca1418e68dfb3a0f068f95cae4bed1ed
Laura Breiman <lauracle@mit.edu>
2015-05-12 11:25:01 -0400

    main menu javascript

--- mainMenu.js
+++ mainMenu.js
@@ -13,23 +13,31 @@ $(document).ready(function(){
         localStorage.setItem("TutorialOn", "false");
     }
     
-    $(".settingsButton").on("click", function(){        
+    $(".settingsButton").on("click", function(){
         if(localStorage.getItem("soundsOn") === "true"){
-            $("#soundsOn").addClass("active");
-            $("#soundsOn").prop("checked","checked");   
+            $("#soundsOn").parent().addClass("active");
+            $("#soundsOn").prop("checked","checked"); 
+            $("#soundsOff").parent().removeClass("active");
+            $("#soundsOff").prop("checked","unchecked");  
         }
         else{
-            $("#soundsOff").addClass("active");
-            $("#soundsOff").prop("checked","checked");           
+            $("#soundsOff").parent().addClass("active");
+            $("#soundsOff").prop("checked","checked"); 
+            $("#soundsOn").parent().removeClass("active");
+            $("#soundsOn").prop("checked","unchecked");  
         }
 
         if(localStorage.getItem("TutorialOn") === "true"){
-            $("#tutorialOn").addClass("active");
+            $("#tutorialOn").parent().addClass("active");
             $("#tutorialOn").prop("checked","checked");   
+            $("#tutorialOff").parent().removeClass("active");
+            $("#tutorialOff").prop("checked","unchecked"); 
         }
         else{
-            $("#tutorialOff").addClass("active");
+            $("#tutorialOff").parent().addClass("active");
             $("#tutorialOff").prop("checked","checked"); 
+            $("#tutorialOn").parent().removeClass("active");
+            $("#tutorialOn").prop("checked","unchecked"); 
         }
     });
 
@@ -44,8 +52,3 @@ $(document).ready(function(){
     })
 
 });
-
-$('#settingsModal').on('hidden.bs.modal', function () {
-    localStorage.setItem("soundsOn", $('#soundsOn').prop('checked'));
-    localStorage.setItem("TutorialOn", $('#tutorialOn').prop('checked'));
-})
